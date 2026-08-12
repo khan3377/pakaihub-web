c5edf8d22153a15af89c4a15055ca21574c0e84c
Khan <mahtaballi@gmail.com>
2026-08-12 15:48:36 +0500

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://pakaihub-web.vercel.app/sitemap.xml",
+   sitemap: "https://pakaihub-web.vercel.app/sitemap.xml",
   };
 }
